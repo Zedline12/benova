cd8d067873166a341c053550424eaec26c7fce58
Zedline12 <bedomohamed307@gmail.com>
2024-08-27 15:53:59 +0300

    update

--- src/app/shared/components/buttons/rectanglebtn/rectanglebtn.component.ts
+++ src/app/shared/components/buttons/rectanglebtn/rectanglebtn.component.ts
@@ -8,9 +8,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class RectanglebtnComponent {
   @Input() type: 'button' | 'submit' | 'reset' = 'button'; // Button type
   @Input() color: string = 'primary'; // Button color
-  @Input() size: 'small' | 'medium' | 'large' = 'medium'; // Button size
+  @Input() size!: 'small' | 'medium' | 'large'; // Button size
   @Input() disabled: boolean = false; // Disabled state
   @Input() loading: boolean = false; // Loading state
+  @Input() style: string = '';
   @Output() btnclick = new EventEmitter<void>(); // Click event
 
   handleClick() {

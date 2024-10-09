b397f65d48cd354b492f03779ef506be0dcf5138
Zedline12 <bedomohamed307@gmail.com>
2024-10-09 17:20:50 +0300

    update

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import CoursesService from '../../services/courses.service';
-
+import { Router, Event, NavigationStart } from '@angular/router';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
-  constructor(public coursesService: CoursesService) {}
+export class NavbarComponent implements OnInit {
+  public isHomePage!: boolean;
+  constructor(
+    public coursesService: CoursesService,
+    private router: Router,
+  ) {}
+  ngOnInit(): void {
+    this.router.events.subscribe((route: Event) => {
+      if (this.router.url === '/') {
+        this.isHomePage = true;
+      }
+      else {
+        this.isHomePage = false;
+      } 
+    });
+  }
 }
